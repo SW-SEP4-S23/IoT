c1c17e790cc67ed268a65382651706120b740ccd
RinaDeithe <316455@via.dk>
2023-05-03 09:52:26 +0200

    Update task sendData to include sensor reading

--- Firmware0_1/Firmware0_1/main.c
+++ Firmware0_1/Firmware0_1/main.c
@@ -24,10 +24,9 @@
 // Needed for HIH8120 driver initialization
 #include <hih8120.h>
 
-// define three Tasks
+// define Tasks
 void task1(void *pvParameters);
-void task2(void *pvParameters);
-void task3(void *pvParameters);
+void sendData(void *pvParameters);
 
 // define semaphore handle
 SemaphoreHandle_t xTestSemaphore;
@@ -83,59 +82,54 @@ void sendData(void *pvParameters)
 
 	for (;;)
 	{
-		time_t t = time(null);
-  		struct tm tm = *localtime(&t);
-		puts("Uploading Message.");
-  		puts("now: %d-%02d-%02d %02d:%02d:%02d\n", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec);
 
+		xTaskDelayUntil(&xLastWakeTime, xFrequency);
+
+		float temperature;
+		uint16_t* CO2;
+		float humidity;
+
+		// CO2 return code.
+		mh_z19_returnCode_t rc;
+
+		if ( HIH8120_OK != hih8120_wakeup() )
+		{
+       		printf("Could not wake up HIH8120 driver.\n");	
+		}
+
+		if ( HIH8120_OK !=  hih8120_measure() )
+		{
+			printf("Could not measure from HIH8120 driver.\n");
+		}
+
+		while (hih8120_isReady())
+		{
+			sleep(0.06);
+		}
+		
+		printf("Reading Humidity and Temperature.\n");
+		temperature = hih8120_getHumidity();
+		humidity = hih8120_getTemperature();
+
+		rc = mh_z19_takeMeassuring();
+		if (rc != OK)
+		{
+		    printf("Could not measure ");
+		}
+
+		mh_z19_getCo2Ppm(CO2);
+
+		puts("Uploading values %d, %d, %d", temperature, *CO2, humidity)
 		lora_driver_payload_t uplink_payload;
+		// Setting up amount of data points
 		uplink_payload.len = 3;	   // Length of the actual payload
 		uplink_payload.portNo = 1; // The LoRaWANport no to sent the message to
-		uplink_payload.bytes[0] = 20;
-		uplink_payload.bytes[1] = 69;
-		// uplink_payload.bytes[1] = 69 & 0x00FF;
-		uplink_payload.bytes[2] = 45;
-		// uplink_payload.bytes[2] = 45 >> 8;
-		// And send it like this:
-		lora_driver_re
+
+		uplink_payload.bytes[0] = temperature;
+		uplink_payload.bytes[1] = *CO2;
+		uplink_payload.bytes[2] = humidity;
+
 		lora_driver_sendUploadMessage(false, &uplink_payload);
-		xTaskDelayUntil(&xLastWakeTime, xFrequency);
-	}
-}
-//*-----------------------------------------------------------*
-
-void task2(void *pvParameters)
-{
-	TickType_t xLastWakeTime;
-	const TickType_t xFrequency = 500 / portTICK_PERIOD_MS; // 500 ms
-
-	// Initialise the xLastWakeTime variable with the current time.
-	xLastWakeTime = xTaskGetTickCount();
-
-	for (;;)
-	{
-		xTaskDelayUntil(&xLastWakeTime, xFrequency);
-		puts("Task1"); // stdio functions are not reentrant - Should normally be protected by MUTEX
-
-		if (HIH8120_OK != hih8120_wakeup())
-		{
-			// Return fejlkode??
-		}
-		else
-		{
-			hih8120_wakeup();
-		}
-
-		if (HIH8120_OK != hih8120_measure())
-		{
-			// Fejlkode??
-		}
-		else
-		{
-			hih8120_measure();
-		}
-
-		PORTA ^= _BV(PA0);
 	}
 }
 
