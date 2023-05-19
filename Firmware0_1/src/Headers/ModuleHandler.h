178dc027249f16f95bbabe92f5510460fff53ca8
toften05 <jacob.toft2001@gmail.com>
2023-05-19 12:52:02 +0200

    add ModuleHandler.c/h with funktions

---
+++ Firmware0_1/src/Headers/ModuleHandler.h
@@ -0,0 +1,12 @@
+/*
+ * ModuleHandler.h
+ *
+ * Created: 19/05/2023 12.42.49
+ *  Author: jacob
+ */ 
+void startCo2Generator();
+void startVentilation();
+void humidifier_Lower();
+void humidifier_Raise();
+void ac_Raise();
+void ac_Lower();
