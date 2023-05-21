2eab7f2d63ca2ce6f7ab59e2ad47465d3f3de3c9
RinaDeithe <316455@via.dk>
2023-05-21 17:01:45 +0200

    Clean up

--- Firmware0_1/src/Logik/Logik.c
+++ Firmware0_1/src/Logik/Logik.c
@@ -21,6 +21,7 @@ void initialise(void){
 	}
 }
 
+
 void humChecker(void *pvParameters){
 	TickType_t xLastWakeTime;
 	const TickType_t xFrequency = 300000 / portTICK_PERIOD_MS;
@@ -49,66 +50,61 @@ void humChecker(void *pvParameters){
 	}
 	
 }
-	void co2Checker(void *pvParameters){
-		TickType_t xLastWakeTime;
-		const TickType_t xFrequency = 300000 / portTICK_PERIOD_MS;
 
-		xLastWakeTime = xTaskGetTickCount();
-		
-		for(;;){
-			xTaskDelayUntil(&xLastWakeTime, xFrequency);
-			if(xSemaphoreTake(Mutex,pdMS_TO_TICKS(200))==pdTRUE){
-				
-				if (data[2] < logikObj.co2_Lower)
-				{
-					return_co2_raised();
-				}
-				else if (data[2] > logikObj.co2_Upper)
-				{
-					return_co2_lowered();
-				}
-				xSemaphoreGive(Mutex);
-			}
-			else
-			{
-				printf("time out");
-			}
-		}
-		
-	}
+
+void co2Checker(void *pvParameters){
+	TickType_t xLastWakeTime;
+	const TickType_t xFrequency = 300000 / portTICK_PERIOD_MS;
+
+	xLastWakeTime = xTaskGetTickCount();
 	
-	void tempChecker(void *pvParameters){
-		TickType_t xLastWakeTime;
-		const TickType_t xFrequency = 300000 / portTICK_PERIOD_MS;
-
-		xLastWakeTime = xTaskGetTickCount();
-		
-		for(;;){
-			xTaskDelayUntil(&xLastWakeTime, xFrequency);
+	for(;;){
+		xTaskDelayUntil(&xLastWakeTime, xFrequency);
+		if(xSemaphoreTake(Mutex,pdMS_TO_TICKS(200))==pdTRUE){
 			
-			if(xSemaphoreTake(Mutex,pdMS_TO_TICKS(200))==pdTRUE){
-				
-				if (data[1] < logikObj.temp_Lower)
-				{
-					return_temp_raised();
-				}
-				else if (data[1] > logikObj.temp_Raise)
-				{
-					return_temp_lowered();
-				}
-				xSemaphoreGive(Mutex);
-			}
-			else
+			if (data[2] < logikObj.co2_Lower)
 			{
-				printf("time out");
+				return_co2_raised();
 			}
+			else if (data[2] > logikObj.co2_Upper)
+			{
+				return_co2_lowered();
+			}
+			xSemaphoreGive(Mutex);
+		}
+		else
+		{
+			printf("time out");
 		}
 	}
 	
-		
-
-
-
-
+}
 	
 
+void tempChecker(void *pvParameters){
+	TickType_t xLastWakeTime;
+	const TickType_t xFrequency = 300000 / portTICK_PERIOD_MS;
+
+	xLastWakeTime = xTaskGetTickCount();
+	
+	for(;;){
+		xTaskDelayUntil(&xLastWakeTime, xFrequency);
+		
+		if(xSemaphoreTake(Mutex,pdMS_TO_TICKS(200))==pdTRUE){
+			
+			if (data[1] < logikObj.temp_Lower)
+			{
+				return_temp_raised();
+			}
+			else if (data[1] > logikObj.temp_Raise)
+			{
+				return_temp_lowered();
+			}
+			xSemaphoreGive(Mutex);
+		}
+		else
+		{
+			printf("time out");
+		}
+	}
+}
