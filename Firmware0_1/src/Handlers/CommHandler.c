2c644ea336802d0e742e424d7b741c44455eb095
RinaDeithe <316455@via.dk>
2023-05-21 18:33:14 +0200

    Change how CommHandler works

--- Firmware0_1/src/Handlers/CommHandler.c
+++ Firmware0_1/src/Handlers/CommHandler.c
@@ -3,11 +3,13 @@
 #include <semphr.h>
 #include "../Headers/Logik.h"
 #include "../Headers/SensorReading.h"
+#include "../Headers/DownLinkValidator.h"
 #include <lora_driver.h>
 
 SemaphoreHandle_t Mutex;
 
-extern logikObj;
+extern float currentID;
+extern logik_obj logikObj;
 
 
 void sendData(void *pvParameters)
@@ -31,7 +33,7 @@ void sendData(void *pvParameters)
 		uplink_payload.bytes[0] = sensor_getTemp();
 		uplink_payload.bytes[1] = sensor_getCo2();
 		uplink_payload.bytes[2] = sensor_getHum();
-		uplink_payload.bytes[3] = 1;
+		uplink_payload.bytes[3] = currentID;
 
 		// Sending uplink message.
 		lora_driver_sendUploadMessage(false, &uplink_payload);
@@ -43,15 +45,9 @@ void recieveData(void *pvParameters)
 {
 
 	TickType_t xLastWakeTime;
-	const TickType_t xFrequency = 300000 / portTICK_PERIOD_MS;
+	const TickType_t xFrequency = 150000 / portTICK_PERIOD_MS;
 
 	xLastWakeTime = xTaskGetTickCount();
-
-	lora_driver_resetRn2483(1);
-	vTaskDelay(2);
-	lora_driver_resetRn2483(0);
-	vTaskDelay(150);
-	lora_driver_flushBuffers();
 
 	for (;;)
 	{
@@ -61,20 +57,26 @@ void recieveData(void *pvParameters)
 		lora_driver_payload_t downlinkPayload;
 
 		xMessageBufferReceive(downLinkMessageBufferHandle, &downlinkPayload, sizeof(lora_driver_payload_t), portMAX_DELAY);
-		printf("DOWN LINK: from port: %d with %d bytes received!", downlinkPayload.portNo, downlinkPayload.len); // Just for Debug
-		if (2 < downlinkPayload.len)
-		{ // Check that we have got the expected 7 bytes
-			// decode the payload into our variales
-			logik.temp_Lower = downlinkPayload.bytes[0];
-			logik.temp_Upper = downlinkPayload.bytes[1];
-			logik.co2_Lower = downlinkPayload.bytes[2];
-			logik.co2_Upper = downlinkPayload.bytes[3];
-			logik.hum_Lower = downlinkPayload.bytes[4];
-			logik.hum_Upper = downlinkPayload.bytes[5];
-			logik.id = (downlinkPayload.bytes[6]);
+		if (6 < downlinkPayload.len)
+		{
 
-			// printf for test
-			printf("The current max temp setting is: %d and uniq id is: %d", logik.temp_Lower, logik.id);
+			// Checks if all the numbers are validated
+
+			if (validateDownlink(downlinkPayload.bytes[4], downlinkPayload.bytes[5], downlinkPayload.bytes[2], downlinkPayload.bytes[3], downlinkPayload.bytes[0], downlinkPayload.bytes[1], downlinkPayload.bytes[6]) == 0)
+			{
+				// if not, returns 1
+				return 1;
+			}
+
+			// Sets the values to the logikObj
+
+			logikObj.co2_Lower = downlinkPayload.bytes[2];
+			logikObj.co2_Upper = downlinkPayload.bytes[3];
+			logikObj.hum_Upper = downlinkPayload.bytes[4];
+			logikObj.hum_Lower = downlinkPayload.bytes[5];
+			logikObj.temp_Lower = downlinkPayload.bytes[0];
+			logikObj.temp_Upper = downlinkPayload.bytes[1];
+			currentID = = downlinkPayload.bytes[6];
 		}
 	}
 }
