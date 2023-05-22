cee58bd2649a1d38402cf24de0120bdb1a093c0e
toften05 <jacob.toft2001@gmail.com>
2023-05-22 13:01:32 +0200

    HotFix

--- Firmware0_1/src/Headers/Logik.h
+++ Firmware0_1/src/Headers/Logik.h
@@ -1,3 +1,9 @@
+#ifndef LOGIK_H
+#define LOGIK_H
+#include <ATMEGA_FreeRTOS.h>
+#include <task.h>
+#include <semphr.h>
+
 typedef struct {
 	int co2_Upper;
 	int co2_Lower;
@@ -19,3 +25,5 @@ void humChecker(void *pvParameters);
 void co2Checker(void *pvParameters);
 
 void tempChecker(void *pvParameters);
+
+#endif  // LOGIK_H
