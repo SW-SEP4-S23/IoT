004d5b3341df793d4874befde7f14ddeda4986eb
RinaDeithe <316455@via.dk>
2023-05-23 22:12:14 +0200

    fix minor issue

--- Firmware0_1/src/Headers/data_handler.h
+++ Firmware0_1/src/Headers/data_handler.h
@@ -19,11 +19,11 @@ uint8_t xData_getTemp_lower();
 uint8_t xData_getTemp_upper();
 uint8_t xData_getId();
 
-uint8_t xData_getCo2_upper();
-uint8_t xData_getCo2_lower();
-uint8_t xData_getHum_upper();
-uint8_t xData_getHum_lower();
-uint8_t xData_getTemp_upper();
-uint8_t xData_getTemp_lower();
+void xData_setCo2_upper(uint8_t value);
+void xData_setCo2_lower(uint8_t value);
+void xData_setHum_upper(uint8_t value);
+void xData_setHum_lower(uint8_t value);
+void xData_setTemp_upper(uint8_t value);
+void xData_setTemp_lower(uint8_t value);
 
 #endif  // DATA_HANDLER_H
