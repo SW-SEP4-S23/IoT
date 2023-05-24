e5db4513309c8493064ed9e702d47b919eae541b
RinaDeithe <316455@via.dk>
2023-05-24 07:02:53 +0200

    create util.h

---
+++ Firmware0_1/src/Headers/util.h
@@ -0,0 +1,15 @@
+#ifndef UTIL_H
+#define UTIL_H
+
+#include <hih8120.h>
+#include <mh_z19.h>
+#include <semphr.h>
+
+char* mh_z19_xReturnCodes_to_text(mh_z19_returnCode_t returnMessage);
+char* hih8120_xReturnCodes_to_text(hih8120_driverReturnCode_t returnMessage);
+
+extern SemaphoreHandle_t printMutex;
+
+void protected_printf(const char* _fmt, ...);
+
+#endif  // UTIL_H
