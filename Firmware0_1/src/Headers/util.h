2d12c1e29a53c6c7efb1491c6f5493301558c892
RinaDeithe <316455@via.dk>
2023-05-24 15:07:33 +0200

    add libraries

--- Firmware0_1/src/Headers/util.h
+++ Firmware0_1/src/Headers/util.h
@@ -3,7 +3,9 @@
 
 #include <hih8120.h>
 #include <mh_z19.h>
+#include <ATMEGA_FreeRTOS.h>
 #include <semphr.h>
+#include <task.h>
 
 char* mh_z19_xReturnCodes_to_text(mh_z19_returnCode_t returnMessage);
 char* hih8120_xReturnCodes_to_text(hih8120_driverReturnCode_t returnMessage);
